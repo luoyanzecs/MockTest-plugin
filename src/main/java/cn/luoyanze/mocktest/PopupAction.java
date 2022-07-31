7c4d66b8a5972b8adf0789157403e4e22d13137a
luoyanzecs <44136414+luoyanzecs@users.noreply.github.com>
2022-07-31 22:41:31 +0800

    initial

---
+++ src/main/java/cn/luoyanze/mocktest/PopupAction.java
@@ -0,0 +1,12 @@
+package cn.luoyanze.mocktest;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+public class PopupAction extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        // TODO: insert action logic here
+    }
+}
